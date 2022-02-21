09932dbf451497d68cb597aee62b4d049751ffe3
Pedro <paglarra@gmail.com>
2022-02-21 09:59:00 -0700

    Added a Resize and Clear button to the Grid.

--- script.js
+++ script.js
@@ -1,28 +1,67 @@
-const mainbody = document.querySelector("#etch-a-sketch");
+const mainbody = document.querySelector("#container");
 
-const gridCell = document.createElement('div')
-gridCell.classList.add('grid')
 
-let cellsHeightandWidth = 16
+function creategrid(x) {
+let cellsHeightandWidth = x
 let cellSize = 600/cellsHeightandWidth
 for(var i = 0; i < cellsHeightandWidth; i++) {
     for(var j = 0; j< cellsHeightandWidth; j++) {
         var div = document.createElement("div");
         div.style.width = cellSize + "px";
         div.style.height = cellSize + "px";
-        div.style.background = "white";
         div.style.display = "inline-block";
         div.style.outline = "1px solid black";
         div.style.margin = 0;
         div.style.border = 0;
 
         var jump = document.createElement("br");
-        document.getElementById("container").appendChild(div);
+        document.getElementById("container").appendChild(div)
+        
     }
 }
+makeHoverable()
+}
 
+function makeHoverable() {
+    const nodes = document.getElementById('container').childNodes;
+    for(let i=0; i<nodes.length; i++) {
+    if (nodes[i].nodeName.toLowerCase() == 'div') {
+        nodes[i].addEventListener('mouseenter', e => nodes[i].classList.add('hoveredCell'))
+     }
+}
+}
 
 
 function resizeGrid () {
-
+    let newSize = prompt("Input a new size for the Grid.")
+    if (newSize > 64) {
+        alert("Please input a size below or equal to 64x64!")
+    }
+    else{
+    mainbody.innerHTML= ""
+    creategrid(newSize)
+    }
 }
+
+function clearGrid() {
+    let gridSize = Math.sqrt(mainbody.getElementsByTagName('div').length);
+    mainbody.innerHTML=''
+    console.log(gridSize)
+    creategrid(gridSize);
+}
+
+
+
+let resizeButton = document.querySelector ("#GridResize");
+resizeButton.addEventListener('click', function() {
+    resizeGrid();
+});
+
+let clearButton = document.querySelector ("#ClearButton");
+clearButton.addEventListener('click', function() {
+    clearGrid();
+});
+
+
+creategrid(16)
+
